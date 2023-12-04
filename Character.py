c1ca408bebaf0f0aa2e2a3bd7e50c7997b5fe8cb
oyc907 <oyc907@naver.com>
2023-12-04 17:37:44 +0900

    12/04 Enemy 부딪힘 구현

--- Character.py
+++ Character.py
@@ -44,13 +44,13 @@ class Character:
 
     def collision_check(self, enemys):
         for enemy in enemys:
-            collision = self.overlap(self.position, enemy.position)
+            collision = self.overlap(self.center, enemy.center, enemy.die_flag)
             
             if collision:
                 enemy.state = 'die'
                 self.state = 'hit'
 
-    def overlap(self, ego_position, other_position):
+    def overlap(self, ego_center, other_center,other_die_flag):
         '''
         두개의 사각형(bullet position, enemy position)이 겹치는지 확인하는 함수
         좌표 표현 : [x1, y1, x2, y2]
@@ -59,5 +59,38 @@ class Character:
             True : if overlap
             False : if not overlap
         '''
-        return ego_position[0] > other_position[0] and ego_position[1] > other_position[1] \
-                 and ego_position[2] < other_position[2] and ego_position[3] < other_position[3]
+        # return ego_position[0] > other_position[0] and ego_position[1] > other_position[1] \
+        #          and ego_position[2] < other_position[2] and ego_position[3] < other_position[3]
+        if not other_die_flag and ego_center[0]<other_center[0] and ego_center[1]>other_center[1]:
+        # 주인공의 오른쪽 위에 Enemy가 존재
+        # not other_die_flag를 하는 이유는 닿는 판정을 한번만 하기위해서
+            if (ego_center[0]+20)>(other_center[0]-25) and (ego_center[1]-20)<(other_center[1]+25):
+            # 부딪혔을 때
+                print ("wow ego_center[0],[1]: ",ego_center[0],ego_center[1])
+                print("wow other_center[0],[1]: ",other_center[0],other_center[1])
+                
+                return 1 
+            else:
+                return 0
+        if not other_die_flag and ego_center[0]>other_center[0] and ego_center[1]>other_center[1]:
+        # 주인공의 왼쪽 위에 Enemy가 존재
+            if (ego_center[0]-20)<(other_center[0]+25) and (ego_center[1]-20)<(other_center[1]+25):
+            # 부딪혔을 때
+                return 1 
+            else:
+                return 0
+        
+        if not other_die_flag and ego_center[0]>other_center[0] and ego_center[1]<other_center[1]:
+        #주인공의 왼쪽 아래에 Enemy가 존재
+            if (ego_center[0]-20)<(other_center[0]+25) and (ego_center[1]+20)>(other_center[1]-25):
+            # 부딪혔을 때
+                return 1 
+            else:
+                return 0
+        if not other_die_flag and ego_center[0]<other_center[0] and ego_center[1]<other_center[1]:
+        #주인공의 오른쪽 아래에 Enemy가 존재
+            if (ego_center[0]+20)>(other_center[0]-25) and (ego_center[1]+20)>(other_center[1]-25):
+            # 부딪혔을 때
+                return 1 
+            else:
+                return 0
